224f7d82ed9dc6ad30d119920e975931859cca8c
Eunsub Lee <ssamko@kakao.com>
2023-07-11 12:26:16 +0900

    conditional_stmt

--- shell_scripts/advanced/conditional_stmt_expanded.sh
+++ shell_scripts/advanced/conditional_stmt_expanded.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-directory="backup"
+directory="backup1"
 
-if [[ -d "$directory" && -z "$(ls -A "$directory")" ]]; then
-    echo "디렉토리 '$directory'은 비어있습니다."
+if [[ -d "$directory" && -n "$(ls -A "$directory")" ]]; then
+    echo "디렉토리 '$directory'은 비어있지 않습니다."
 else
-    echo "디렉토리 '$directory'은 비어있지 않거나 존재하지 않습니다."
+    echo "디렉토리 '$directory'은 비어있거나 존재하지 않습니다."
 fi

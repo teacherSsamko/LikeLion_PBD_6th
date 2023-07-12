f30d503a485b07d094177238504ffa8e1113b906
Eunsub Lee <ssamko@kakao.com>
2023-07-12 18:27:48 +0900

    update arg_study

--- shell_scripts/advanced3/arg_study.sh
+++ shell_scripts/advanced3/arg_study.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-echo "Hello, $1!"
-echo "You too, $2!"
-echo "Have a nice day, $3!"
+if [ $# -eq 0 ]; then
+    echo "Oops. There is no one to say hello.."
+    echo "사용법: $0 <사람1> <사람2> ..."
+    exit 1
+fi
+
+comments=("Hello, " "You too, " "Have a nice day, ")
+
+i=0
+for arg in "$@"; do
+    echo "${comments[${i}]} $arg!"
+    i=$((i+1))
+done
 
